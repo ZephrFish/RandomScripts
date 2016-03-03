a0d92af881c31e5d9eb5364c425d120ec73d554b
ZephrFish <5783068+ZephrFish@users.noreply.github.com>
2016-03-03 14:16:46 +0000

    Added Comment description

--- masslookup.sh
+++ masslookup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Mass NSLookup Script
-
+# Where $1 = list of domain names & $2 = your dns server#53
 for ip in $(cat $1); do nslookup $ip | grep Address: | cut -d ":" -f 2| sort| uniq | grep -v
 "$2"; done
