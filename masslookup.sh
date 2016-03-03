db09b24a47207eeeed0532f305cd080825152313
ZephrFish <ZephrFish@users.noreply.github.com>
2016-03-03 16:08:17 +0000

    added args detection
    added args detection

--- masslookup.sh
+++ masslookup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Mass NSLookup Script
 # Where $1 = list of domain names & $2 = your dns server#53
+if [ -z "$2" ];
+then
+echo ""
+echo "   Usage: ./masslookup.sh urls.txt <dns_ip#53>"
+echo ""
+else
 for ip in $(cat $1); do nslookup $ip | grep Address: | cut -d ":" -f 2| sort| uniq | grep -v
 "$2"; done
