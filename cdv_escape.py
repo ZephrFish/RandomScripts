1120a0c4e433d29960808016800df756f92f402a
ZephrFish <5783068+ZephrFish@users.noreply.github.com>
2016-07-25 17:30:03 +0100

    Create cdv_escape.py

---
+++ cdv_escape.py
@@ -0,0 +1,10 @@
+def escape(payload):
+     if payload[0] in ('@','+','-', '=', '|'):
+             payload = payload.replace("|", "\|")
+             payload = "'" + payload + "'"
+     return payload
+
+# An example of payload string
+payload = "@cmd|' /C calc'!A0"
+print "The Unescaped version is: " +  payload
+print "When passed through escape function the value is: " + escape(payload)
