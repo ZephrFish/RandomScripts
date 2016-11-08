a5a80882b3d9e89c767230a11ac53138b5e62750
ZephrFish <5783068+ZephrFish@users.noreply.github.com>
2016-11-08 21:43:35 +0000

    Update screener.sh

--- screener.sh
+++ screener.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-echo "Screenshot Tool"
-echo "Written by ZephrFish"
-mkdir $1_outputs
-for i in $(cat $1); 
-    do 
-      xvfb-run cutycapt --url=https://$i --out=$1_outputs/https_$i.png --out-format=png; 
-      xvfb-run cutycapt --url=http://$i --out=$1_outputs/http_$i.png --out-format=png; 
-done
-tar czvf $1_screens.tar.gz $1_outputs/*
-rm -rf $1_outputs
-echo "[!] Finished..."
+# Does not work, ignore if you're sane
+#echo "Screenshot Tool"
+#echo "Written by ZephrFish"
+#mkdir $1_outputs
+#for i in $(cat $1); 
+#    do 
+#      xvfb-run cutycapt --url=https://$i --out=$1_outputs/https_$i.png --out-format=png; 
+#      xvfb-run cutycapt --url=http://$i --out=$1_outputs/http_$i.png --out-format=png; 
+#done
+#tar czvf $1_screens.tar.gz $1_outputs/*
+#rm -rf $1_outputs
+#echo "[!] Finished..."
