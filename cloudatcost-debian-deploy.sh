4a8cf3ea5daac730305499f3c0d51e3a85019524
ZephrFish <5783068+ZephrFish@users.noreply.github.com>
2016-11-08 21:39:55 +0000

    Create cloudatcost-debian-deploy.sh

---
+++ cloudatcost-debian-deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Debian Build Script 
+# Cloud at Cost
+# ZephrFish
+echo "[+] Enter a username to start deploying"
+adduser $1
+echo "[+] Debian Deploy Script"
+rm -rf /etc/apt/sources.list
+touch /etc/apt/sources.list
+echo "# Debian 9" >> /etc/apt/sources.list
+echo "deb http://ftp.debian.org/debian testing main contrib non-free" >> /etc/apt/sources.list
+echo "deb-src http://ftp.debian.org/debian testing main contrib non-free" >> /etc/apt/sources.list
+echo "deb http://ftp.debian.org/debian/ stretch-updates main contrib non-free" >> /etc/apt/sources.list
+echo "deb-src http://ftp.debian.org/debian/ stretch-updates main contrib non-free" >> /etc/apt/sources.list
+echo "deb http://security.debian.org/ stretch/updates main contrib non-free" >> /etc/apt/sources.list
+echo "deb-src http://security.debian.org/ stretch/updates main contrib non-free" >> /etc/apt/sources.list
+apt-get update
+apt-get upgrade -y
+apt-get dist-upgrade -y
+apt install sudo wget curl git zip ccze byobu zsh -y
+gpasswd -a $1 sudo
+pvscan && vgextend localhost-vg /dev/sda3 && lvextend -l +100%FREE /dev/localhost-vg/root && resize2fs /dev/localhost-vg/root
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
