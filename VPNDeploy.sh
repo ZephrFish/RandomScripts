1d04ec796a07257dd2338d187b79f17ee82750d4
ZephrFish <ZephrFish@users.noreply.github.com>
2016-11-19 18:11:45 +0000

    Update VPNDeploy.sh

--- VPNDeploy.sh
+++ VPNDeploy.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 # OpenVPN Debian Deployment Script
 # ZephrFish
-# Mishmash of scripts, parts from Road Warrior OpenVPN
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Mishmash of scripts, parts from Road Warrior OpenVPN(https://github.com/Nyr/openvpn-install), parts from Deployment Script(https://github.com/ZephrFish/RandomScripts/blob/master/cloudatcost-debian-deploy.sh)
+# Other parts purely written :-)
+# Enjoy
+# The MIT License
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 echo "[+] Debian Deploy Script"
 echo "Sets up a user, installs and deploys openvpn"
@@ -45,11 +55,6 @@ fi
 if [[ ! -e /dev/net/tun ]]; then
 	echo "TUN is not available"
 	exit 3
-fi
-
-if grep -qs "CentOS release 5" "/etc/redhat-release"; then
-	echo "CentOS 5 is too old and not supported"
-	exit 4
 fi
 if [[ -e /etc/debian_version ]]; then
 	OS=debian
