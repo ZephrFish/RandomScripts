7454575bce331c71ff9baf6f032f51eb6a52b175
ZephrFish <ZephrFish@users.noreply.github.com>
2017-10-13 20:26:16 +0100

    Update FileChecker.sh

--- FileChecker.sh
+++ FileChecker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # File Checker
 # A quick script for checking a list of IPs or URLs for a specific file
-# Usage: ./FileChecker.sh targets.txt phpinfo.php
+# Usage: ./FileChecker.sh <targetsFile> <fileToLookFor>
+# Example: ./FileChecker.sh targets.txt phpinfo.php
 
 for x in $(cat $1);
 do
