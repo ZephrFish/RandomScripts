97f048c35443a2a98d065f71820893efb9a39dfd
ZephrFish <ZephrFish@users.noreply.github.com>
2017-10-13 20:38:13 +0100

    Create Multi-iRecon.sh

---
+++ Multi-iRecon.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Mutli-Instance iRecon
+# mkdir -p  /usr/share/tools/
+# git clone https://github.com/ZephrFish/iRecon
+# export PATH= /usr/share/tools/iRecon/
+for x in $(cat IPs.txt); do iRecon.py $x | tee $x_iRecon.txt; done
