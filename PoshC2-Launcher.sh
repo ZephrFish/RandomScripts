0c4793d1e41678acb9d615cb1fb1bea869b26871
ZephrFish <5783068+ZephrFish@users.noreply.github.com>
2020-01-22 18:08:58 +0000

    Create PoshC2-Launcher.sh

---
+++ PoshC2-Launcher.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Posh Launcher
+# req byobu && PoshC2
+# sudo apt install byobu
+# curl -sSL https://raw.githubusercontent.com/nettitude/PoshC2/master/Install.sh | bash
+# First we create a new sesson, this can be called anything
+byobu new-session -d -s "PoshC2"
+
+# Next create a split window vertically into two halves
+byobu split-window -v
+
+# Setup posh-server on top halve
+byobu send-keys -t 0 "posh-server" Enter
+
+# Setup posh implant handler in bottom window
+byobu send-keys -t 1 "posh -u Andy-Testing" Enter
+
+# Select newly created window
+byobu attach-session -t "PoshC2"
+
