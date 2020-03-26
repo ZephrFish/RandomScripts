fba1a67b76e3c05c87b8fa2df7ccd9d9cb159fd9
ZephrFish <5783068+ZephrFish@users.noreply.github.com>
2020-03-26 01:02:06 +0000

    Runs in windows python

---
+++ WakeTFUp.py
@@ -0,0 +1,58 @@
+import pyaudio
+import math
+import struct
+import wave
+import time
+import os
+import win32com.client as wincl
+
+Threshold = 10
+
+SHORT_NORMALIZE = (1.0/32768.0)
+chunk = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+swidth = 2
+
+class Recorder:
+
+    @staticmethod
+    def rms(frame):
+        count = len(frame) / swidth
+        format = "%dh" % (count)
+        shorts = struct.unpack(format, frame)
+
+        sum_squares = 0.0
+        for sample in shorts:
+            n = sample * SHORT_NORMALIZE
+            sum_squares += n * n
+        rms = math.pow(sum_squares / count, 0.5)
+
+        return rms * 1000
+
+    def __init__(self):
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(format=FORMAT,
+                                  channels=CHANNELS,
+                                  rate=RATE,
+                                  input=True,
+                                  output=True,
+                                  frames_per_buffer=chunk)
+
+    def record(self):
+        speak = wincl.Dispatch("SAPI.SpVoice")
+        speak.Speak("WAKE the fuck up")
+
+    def listen(self):
+        print('Listening beginning')
+        while True:
+            input = self.stream.read(chunk)
+            rms_val = self.rms(input)
+            if rms_val > Threshold:
+                self.record()
+
+a = Recorder()
+
+a.listen()
+
