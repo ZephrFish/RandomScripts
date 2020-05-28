7f5a3f1c897eee9f3c1e16677735c3045a97654b
ZephrFish <5783068+ZephrFish@users.noreply.github.com>
2020-05-28 04:18:22 +0100

    Create sslscan2XML.py

---
+++ sslscan2XML.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+# coding: utf-8
+# vim:ts=4 sts=4 tw=100:
+
+import sys
+import re
+a = open(sys.argv[1]).read()
+m = re.search('Testing SSL server ([\d.]+) on port (\d+)', a, re.S)
+
+print """<?xml version="1.0" encoding="UTF-8"?>
+<document title="SSLScan Results" version="1.8.2" web="http://www.titania.co.uk">
+<ssltest host="%s" port="%s">""" % m.groups()
+
+for b in re.findall(
+    'Accepted.*?(TLSv1|SSLv2|SSLv3)\s+(\d+) bits\s+([\w-]+)',
+        a):
+    print '<cipher status="accepted" sslversion="%s" bits="%s" cipher="%s" />' % b
+
+print "</ssltest></document>"
