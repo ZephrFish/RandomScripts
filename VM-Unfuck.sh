311a332aef07861acf532b06a4fc8f02888c4830
ZephrFish <ZephrFish@users.noreply.github.com>
2020-06-26 10:20:42 +0100

    Create VM-Unfuck.sh

---
+++ VM-Unfuck.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+processString=$(ps -ef | grep '[0-9][0-9]:[0-9][0-9]:[0-9][0-9] /usr/bin/vmtoolsd -n vmusr') # get process info for vmtoolsd
+tokens=( $processString ) # tokenize
+kill "${tokens[1]}" # grab pid and kill it
+/usr/bin/vmtoolsd -n vmusr & > /dev/null 2>&1 # restart vmtoolsd
