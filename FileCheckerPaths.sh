23de6b03de4103753bf0a994981889a2cf008f00
ZephrFish <ZephrFish@users.noreply.github.com>
2021-02-28 23:25:23 +0000

    added output to file and check validity

--- FileCheckerPaths.sh
+++ FileCheckerPaths.sh
@@ -4,11 +4,12 @@
 # Usage: ./FileChecker.sh <targetsFile> <fileToLookFor>
 # Example: ./FileChecker.sh targets.txt phpinfo.php
 
-for x in $(cat $1);
+for x in $(<$1);
 do
 if curl --output /dev/null --head --silent --fail "$x/$2";
 then
 echo "URL exists: $x/$2"
+echo "$x/$2" | httprobe >> ValidURLs.txt
 else
 echo "URL does not exist: $x/$2"
 fi;
